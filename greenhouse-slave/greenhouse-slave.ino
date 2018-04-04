81fe0b4ac10947bb355b195562930f09f6b1e8af
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2018-04-05 01:49:55 +0200

    feat(): add a basic impl for slave

--- greenhouse-slave/greenhouse-slave.ino
+++ greenhouse-slave/greenhouse-slave.ino
@@ -2,42 +2,33 @@
 #include <Time.h>
 #include <TimeAlarms.h>
 
-boolean ledToggle = false;
-const int ledPin2 = 2;
-const int ledPin3 = 3;
-const int ledPin4 = 4;
-const int ledPin5 = 5;
-const int ledPin6 = 6;
 const int lightSensorPin = A0;
+
+const int relayNeon1Pin = 4;
+const int relayNeon2Pin = 5;
+const int relayFanPin = 6;
+boolean relayFanToggle = false;
+
 int lightValue;
 
-int relayPin = 10;
-boolean relayToggle = false;
-
-const int tmpSensorPin = A1;
-int tempValue;
-
-const int termoResistorPin = A2;
-double termoValue;
-
+#define DHT22_PIN 8
 dht DHT;
 
-#define DHT22_PIN 8
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(relayPin, OUTPUT);
+  pinMode(relayNeon1Pin, OUTPUT);
+  pinMode(relayNeon2Pin, OUTPUT);
+  pinMode(relayFanPin, OUTPUT);
 
-  setTime(1,23,00,22,3,18); // set time to Saturday 8:29:00am Jan 1 2011
+  setTime(1, 23, 00, 22, 3, 18); // set time (FIXME replace with a real time)
 
-  Alarm.alarmRepeat(1,24,0, toggleNeon);  // 8:30am every day
-  Alarm.alarmRepeat(1,25,0, toggleNeon);  // 5:45pm every day 
-  
-  Alarm.timerRepeat(10, readTemp); 
-  Alarm.timerRepeat(15, readDht); 
-  Alarm.timerRepeat(5, toggleLeds); 
-  Alarm.timerRepeat(2, readLight);  
+  Alarm.alarmRepeat(1, 23, 20, enableNeons); // FIXME change to 7:30am
+  Alarm.alarmRepeat(1, 24, 5, disableNeons); // FIXME change to 9:00pm
+
+  Alarm.timerRepeat(58, updateFan); // FIXME this should be every 15 minutes
+  Alarm.timerRepeat(3, readDht);
+  Alarm.timerRepeat(3, readLight);
 }
 
 void loop() {
@@ -49,21 +40,6 @@ void readLight() {
   lightValue = analogRead(lightSensorPin);
   Serial.print("Value light: \t");
   Serial.println(lightValue); // ????
-}
-
-void readTemp() {
-  int tempSensorVal = analogRead(tmpSensorPin);
-  float tempVoltage = (tempSensorVal / 1024.0) * 5.0;
-  tempValue = (tempVoltage - .5) * 100;
-  Serial.print("Voltage: \t");
-  Serial.print(tempVoltage);
-  Serial.print("\t Temp value: \t");
-  Serial.println(tempValue);
-  
-  // termo resistenza
-  termoValue = calcTemp(analogRead(A2), 3950, 2800);
-  Serial.print("\t Termo value: \t");
-  Serial.println(termoValue);
 }
 
 void readDht() {
@@ -79,57 +55,39 @@ void readDht() {
   Serial.println("°C");
 }
 
-void toggleNeon() {
-  if (relayToggle == true) {
-    digitalWrite(relayPin, LOW);
-    relayToggle = false;
+void enableNeons() {
+  digitalWrite(relayNeon1Pin, HIGH);
+  digitalWrite(relayNeon2Pin, HIGH);
+}
+
+void disableNeons() {
+  digitalWrite(relayNeon1Pin, LOW);
+  digitalWrite(relayNeon2Pin, LOW);
+}
+
+void updateFan() {
+  if (relayFanToggle == true) {
+    digitalWrite(relayFanPin, HIGH);
+    relayFanToggle = false;
   }
   else {
-    relayToggle = true;
-    digitalWrite(relayPin, HIGH);
+    relayFanToggle = true;
+    digitalWrite(relayFanPin, HIGH);
   }
 }
 
-void toggleLeds() {
-  Serial.print("toggleLeds");
-  if (ledToggle == false) {
-    ledToggle = true;
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin3, HIGH);
-    digitalWrite(ledPin4, HIGH);
-    digitalWrite(ledPin5, HIGH);
-    digitalWrite(ledPin6, HIGH);
-  } else {
-    ledToggle = false;
-    digitalWrite(ledPin2, LOW);
-    digitalWrite(ledPin3, LOW);
-    digitalWrite(ledPin4, LOW);
-    digitalWrite(ledPin5, LOW);
-    digitalWrite(ledPin6, LOW);
-  }
-}
 
-double calcTemp(int value, int B, int R0)
-{
-  double V=(5/1023.00)*value;
-  double R=((10000.00*5)/V)-10000;
-  double T=B/log(R/(R0*pow(M_E,(-B/298.15))));
-  return T-273.15;
-}
-
-void digitalClockDisplay()
-{
+void digitalClockDisplay() {
   // digital clock display of the time
   Serial.print(hour());
   printDigits(minute());
   printDigits(second());
-  Serial.println(); 
+  Serial.println();
 }
 
-void printDigits(int digits)
-{
+void printDigits(int digits) {
   Serial.print(":");
-  if(digits < 10)
+  if (digits < 10)
     Serial.print('0');
   Serial.print(digits);
 }
